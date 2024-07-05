b2d5b89df775dcd19922c7b64896958e29afbe6a
LAlinaV <ladik151104@gmail.com>
2024-07-05 15:03:44 +0300

    connect views

--- Dialog/app/src/main/java/com/example/dialog/MainActivity.java
+++ Dialog/app/src/main/java/com/example/dialog/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.dialog;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RelativeLayout;
-
-public class MainActivity extends AppCompatActivity {
+import android.widget.Toast;
+public class MainActivity extends Activity implements OnClickListener {
     private Button bgButton;
     public RelativeLayout relativeLayout;
     Context context;
@@ -16,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bgButton = (Button) findViewById(R.id.background_button);
+        bgButton.setOnClickListener(this);
+        context = MainActivity.this;
+        relativeLayout = (RelativeLayout)findViewById(R.id.relativelayout);
     }
+
 }
