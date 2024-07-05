80353e78adb104561db27be0a199912165577198
LAlinaV <ladik151104@gmail.com>
2024-07-05 14:58:42 +0300

    add variables in MainActivity

--- Dialog/app/src/main/java/com/example/dialog/MainActivity.java
+++ Dialog/app/src/main/java/com/example/dialog/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.dialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bgButton;
+    public RelativeLayout relativeLayout;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
