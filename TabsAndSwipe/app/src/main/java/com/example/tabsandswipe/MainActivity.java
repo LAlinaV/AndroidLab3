c3f35980b873d175b0c2e7ce39fca0ec384608d7
LAlinaV <ladik151104@gmail.com>
2024-07-06 14:53:26 +0300

    add ViewTab, adapter

--- TabsAndSwipe/app/src/main/java/com/example/tabsandswipe/MainActivity.java
+++ TabsAndSwipe/app/src/main/java/com/example/tabsandswipe/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.tabsandswipe;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
-import com.google.android.material.tabs.TabLayout;
-
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +22,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         ViewPager2 viewPager = findViewById(R.id.view_pager);
-         TabLayout tabLayout = findViewById(R.id.tab_layout);
+        final ViewPager2 viewPager = findViewById(R.id.viewPager);
+
+        List<Map<String, Object>> mapList = new ArrayList<>();
+
+        Map<String, Object> first_tab = setTabTitleAndFragment("first", new first_tab());
+        Map<String, Object> second_tab = setTabTitleAndFragment("second", new second_tab());
+        Map<String, Object> third_tab = setTabTitleAndFragment("third", new third_tab());
+
+        mapList.add(first_tab);
+        mapList.add(second_tab);
+        mapList.add(third_tab);
+
+        final ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this, mapList);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(viewPagerAdapter.getTitle(position));
+            }
+        }).attach();
+
+    }
+
+    Map<String, Object> setTabTitleAndFragment(String title, Fragment fragment){
+
+        Map<String, Object> fragmentWithTitleMap = new HashMap<>();
+
+        fragmentWithTitleMap.put("fragmentTitle", title);
+        fragmentWithTitleMap.put("fragment", fragment);
+
+        return fragmentWithTitleMap;
     }
 }
